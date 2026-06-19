17599b40d0aec55400d55b6e3e559873adb6ef60
shouzo2 <tiagomanuelsoaresdossantos@gmail.com>
2026-06-19 13:46:21 +0100

    first site

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -37,36 +37,22 @@ const Home = () => {
         Artista Plástico, Pintor & Escultor
       </h2>
 
-      <div className="social-icons">
-        <a href="https://twitter.com/TiagoSoaresdos" target="_blank" rel="noopener noreferrer" title="Twitter">
-          <i className="fab fa-twitter"></i>
-        </a>
 
-        <a href="https://www.facebook.com/profile.php?id=61556461990404" target="_blank" rel="noopener noreferrer" title="Facebook">
-          <i className="fab fa-facebook"></i>
-        </a>
+      
 
-        <a href="https://www.linkedin.com/in/tiago-santos-445876166/" target="_blank" rel="noopener noreferrer" title="LinkedIn">
-          <i className="fab fa-linkedin"></i>
-        </a>
+     
 
-        <a href="https://github.com/shouzo1" target="_blank" rel="noopener noreferrer" title="GitHub">
-          <i className="fab fa-github"></i>
-        </a>
+        
 
         <a href="https://www.instagram.com/zesoares89/" target="_blank" rel="noopener noreferrer" title="Instagram">
           <i className="fab fa-instagram"></i>
         </a>
 
-        <a href="https://www.youtube.com/@Shouzo-euw" target="_blank" rel="noopener noreferrer" title="YouTube">
-          <i className="fab fa-youtube"></i>
-        </a>
+      
 
-        <a href="https://www.behance.net/tiagosantos879" target="_blank" rel="noopener noreferrer" title="Behance">
-          <i className="fa-brands fa-behance"></i>
-        </a>
+      
       </div>
-    </div>
+    
   );
 };
 
